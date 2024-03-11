619a6e2857aa390ebcd6a5c2e984ce6202f11af1
zxt57 <77510237+zxt57@users.noreply.github.com>
2024-03-11 18:30:03 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,26 +1,39 @@
 import socket
-IP = socket.gethostname()
-PORT = 4455
-FORMAT = "utf-8"
-SIZE = 1024
+import threading
+import time
 
-def main():
-  client = socket.socket()
-  client.connect((IP,PORT))
+IP = socket.gethostname()
+PORT = 5678
+SIZE = 1024
+TIMEOUT = 20
+FORMAT = "utf-8"
+
+def send(filename, file_data):
   
-  file = open("data/yt.txt", "r")
-  data = file.read()
+  try:
+    client = socket.socket()
+    client.settimeout(TIMEOUT)
+    client.connect((IP,PORT))
+    client.send(filename.encode(FORMAT))
+    re_msg = client.recv(SIZE).decode(FORMAT)
+    if re_msg != filename:
+      raise Exception(f"Received filename {re_msg} different from {filename}")
+    client.send(file_data)
+
+  except Exception as e:
+    print("Error : ", e)
+    
+  finally:
+    client.close()
+
+
+def thread_send(filename, file_data):
+  threading.Thread(target=send, args=(filename,file_data)).start()
   
-  client.send("yt.txt".encode(FORMAT))
-  msg = client.recv(SIZE).decode(FORMAT)
-  print(f"[SERVER]: {msg}")
-  
-  client.send(data.encode(FORMAT))
-  msg = client.recv(SIZE).decode(FORMAT)
-  print(f"[SERVER]: {msg}")
-  
-  file.close()
-  client.close()
-  
+
 if __name__ == "__main__":
-  main()
+  file = open("test_text.txt", "r")
+  data = file.read()
+  thread_send("test_text.txt", data)
+  file.close()
+  time.sleep(100)
